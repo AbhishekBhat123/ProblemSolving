4e83b6fa78801596e4d5d1a13c463a1cb6398d9d
Abhishek Bhat <144084687+AbhishekBhat123@users.noreply.github.com>
2026-08-14 21:59:55 +0530

    Time: 1 ms (100%), Space: 46.6 MB (82.2%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
@@ -46,45 +46,97 @@
 // }
 // the above approach is good in logic but has more issue 
 
-// this is the good code of the same logic
+// this is the good code of the same logic which has TC: O(mnlogmin(mn)) and SC:(Omin(mn))
+// class Solution {
+//     public int[][] diagonalSort(int[][] mat) {
+    
+//     int m = mat.length;
+//     int n = mat[0].length;
+
+    // for(int startcolumn = 0; startcolumn < n; startcolumn++){
+    //     sort(mat,0,startcolumn,m,n);
+    // }
+
+    // for(int startrow = 1; startrow < m; startrow++){
+    //     sort(mat,startrow,0,m,n);
+    // }
+     
+    //  return mat;
+
+//  }
+
+//     private void sort(int mat[][], int row, int col, int m, int n){
+//         List<Integer> temp = new ArrayList<>();
+//         int r = row;
+//         int c = col;
+
+//         while(r<m && c<n){
+//             temp.add(mat[r][c]);
+//             r++;
+//             c++;
+//         }
+//         int i = 0;
+//         Collections.sort(temp);
+
+//         r = row;
+//         c = col;
+//         while(r<m && c<n){
+//             mat[r][c] = temp.get(i);
+//             i++;
+//             r++;
+//             c++;
+//         }
+//     }
+// }
+
+
+
+// the optimal approach for this will be using count sort
 class Solution {
     public int[][] diagonalSort(int[][] mat) {
-    
-    int m = mat.length;
-    int n = mat[0].length;
+        int m = mat.length;
+        int n = mat[0].length;
 
-    for(int startcolumn = 0; startcolumn < n; startcolumn++){
-        sort(mat,0,startcolumn,m,n);
+        // Diagonals starting from the top row
+        for (int col = 0; col < n; col++) {
+            sortDiagonal(mat, 0, col, m, n);
+        }
+
+        // Diagonals starting from the left column
+        // Start from 1 because (0,0) was already handled
+        for (int row = 1; row < m; row++) {
+            sortDiagonal(mat, row, 0, m, n);
+        }
+
+        return mat;
     }
 
-    for(int startrow = 1; startrow < m; startrow++){
-        sort(mat,startrow,0,m,n);
-    }
-     
-     return mat;
+    private void sortDiagonal(int[][] mat, int row, int col, int m, int n) {
 
- }
+        // 1. Count frequency of each value
+        int[] count = new int[101];
 
-    private void sort(int mat[][], int row, int col, int m, int n){
-        List<Integer> temp = new ArrayList<>();
         int r = row;
         int c = col;
 
-        while(r<m && c<n){
-            temp.add(mat[r][c]);
+        while (r < m && c < n) {
+            count[mat[r][c]]++;
             r++;
             c++;
         }
-        int i = 0;
-        Collections.sort(temp);
 
+        // 2. Put values back in sorted order
         r = row;
         c = col;
-        while(r<m && c<n){
-            mat[r][c] = temp.get(i);
-            i++;
-            r++;
-            c++;
+
+        for (int value = 1; value <= 100; value++) {
+            while (count[value] > 0) {
+                mat[r][c] = value;
+
+                r++;
+                c++;
+                count[value]--;
+            }
         }
     }
 }
