b9a0086f353b2f63b5e371d56f8495e7e5b1707b
Abhishek Bhat <144084687+AbhishekBhat123@users.noreply.github.com>
2026-08-16 10:09:18 +0530

    Time: 1 ms (99.85%), Space: 72.1 MB (29.18%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -21,27 +21,52 @@
 
 // that actually logics correct but the problem is which gives error on example 2
 
+// class Solution {
+//     public int[] productExceptSelf(int[] nums) {
+//         int n = nums.length;
+//         int[] answer = new int[n];
+
+//         // Pass 1: store product of everything to the LEFT
+//         int leftProduct = 1;
+
+//         for (int i = 0; i < n; i++) {
+//             answer[i] = leftProduct;
+//             leftProduct *= nums[i];
+//         }
+
+//         // Pass 2: multiply by product of everything to the RIGHT
+//         int rightProduct = 1;
+
+//         for (int i = n - 1; i >= 0; i--) {
+//             answer[i] *= rightProduct;
+//             rightProduct *= nums[i];
+//         }
+
+//         return answer;
+//     }
+// }
+
+
+
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        int[] answer = new int[n];
+        int ans[] = new int[n];
 
-        // Pass 1: store product of everything to the LEFT
-        int leftProduct = 1;
-
-        for (int i = 0; i < n; i++) {
-            answer[i] = leftProduct;
-            leftProduct *= nums[i];
+        // find left sum of the nums[i]
+        int leftSum = 1;
+        for(int i = 0; i<n; i++){
+            ans[i] = leftSum;
+            leftSum *= nums[i];
         }
 
-        // Pass 2: multiply by product of everything to the RIGHT
-        int rightProduct = 1;
-
-        for (int i = n - 1; i >= 0; i--) {
-            answer[i] *= rightProduct;
-            rightProduct *= nums[i];
+        // to find the right sum 
+        int rightSum = 1;
+        for(int i = n-1; i>=0; i--){
+            ans[i] *= rightSum;
+            rightSum *= nums[i];
         }
 
-        return answer;
+        return ans;
     }
 }
