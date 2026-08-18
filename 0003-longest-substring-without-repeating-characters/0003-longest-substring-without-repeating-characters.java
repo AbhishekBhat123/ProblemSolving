37a63781f063ba5947b12bb699382756518d83f8
Abhishek Bhat <144084687+AbhishekBhat123@users.noreply.github.com>
2026-08-18 21:27:55 +0530

    Time: 5 ms (90.51%), Space: 47.7 MB (26.78%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -3,6 +3,40 @@ class Solution {
         List<Character> list = new ArrayList<>();
 
         int n = s.length();
+        // int max = Integer.MIN_VALUE;
+
+        // for(int i =0; i<n; i++){
+        //     if(!list.contains(s.charAt(i))){
+        //         list.add(s.charAt(i));
+        //         max = Math.max(max, (list.size()));
+        //     }
+        //     else{
+        //         list.clear();
+        //     }
+        // }
+        // return max;
+
+
+        // HashMap<Character, Integer> hm = new HashMap<>();
+        // int left = 0;
+        // int right = 0;
+        // int max = 0;
+
+        // for(right = 0; right<n; right++){
+        //     char ch = s.charAt(right);
+
+        //     if(hm.containsKey(ch)){
+        //         left = Math.max(left, hm.get(ch)+1);
+        //     }
+
+        //     hm.put(ch,right);
+        //     max = Math.max(max, right - left + 1);
+
+        // }
+
+        // return max;
+
+
 
         int[] lastSeen = new int[128];
         Arrays.fill(lastSeen, -1);
