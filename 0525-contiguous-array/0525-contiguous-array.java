7386e4c48916743d21039d5abc790d1968f7183d
Abhishek Bhat <144084687+AbhishekBhat123@users.noreply.github.com>
2026-08-20 20:54:32 +0530

    Time: 23 ms (85.29%), Space: 65.4 MB (77.72%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.java
+++ 0525-contiguous-array/0525-contiguous-array.java
@@ -25,35 +25,48 @@
 //     }
 // }
 
+// this is the optimal brute force approach for contiguos problem
+// class Solution {
+//     public int findMaxLength(int[] nums) {
+//        int n = nums.length;
+//        int contiguos = 0;
+//        for(int i =0; i<n; i++){
+//         int zeros = 0;
+//         int ones = 0;
+//         for(int j = i; j<n; j++){
+//             if(nums[j] == 0){zeros++;}
+//             else{ones++;}
+
+//             if(zeros == ones){contiguos = Math.max(contiguos, zeros+ones);}
+//         }
+//        }
+//        return contiguos;
+//     }
+// }
+
+// optimal
 
 class Solution {
     public int findMaxLength(int[] nums) {
+
+        int maxlength = 0;
+        int n = nums.length;
+        int count = 0;
+
         HashMap<Integer, Integer> map = new HashMap<>();
 
-        // balance 0 exists before the array starts
         map.put(0, -1);
 
-        int balance = 0;
-        int maxLength = 0;
+        for(int i =0; i<n; i++){
+            if(nums[i] == 0){count--;}
+            else{count++;}
 
-        for (int i = 0; i < nums.length; i++) {
-
-            if (nums[i] == 0) {
-                balance--;
-            } else {
-                balance++;
+            if(map.containsKey(count)){
+                maxlength = Math.max(maxlength, i- map.get(count));
             }
-
-            if (map.containsKey(balance)) {
-                // Same balance → equal number of 0s and 1s in between
-                int length = i - map.get(balance);
-                maxLength = Math.max(maxLength, length);
-            } else {
-                // Store only the FIRST occurrence
-                map.put(balance, i);
-            }
+            else{map.put(count, i);}
         }
+        return maxlength;
 
-        return maxLength;
     }
 }
