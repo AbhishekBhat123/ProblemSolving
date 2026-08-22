186bdc8ee13eba620170a62b9507e4521d5fff59
Abhishek Bhat <144084687+AbhishekBhat123@users.noreply.github.com>
2026-08-22 21:47:19 +0530

    Time: 23 ms (93.71%), Space: 48.9 MB (47.25%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -0,0 +1,46 @@
+// class Solution {
+//     public int subarraySum(int[] nums, int k) {
+//         // the brute force approach is to use 2 loops for getting the range of subarrays and we use carry forward approach 
+
+//         int n = nums.length;
+//         int sum;
+//         int cnt = 0;
+//         for(int i = 0; i<n; i++){
+//              sum = 0;
+//             for(int j = i; j<n; j++){
+//                 sum += nums[j];
+//                 if(sum == k){cnt++;}
+//             }
+//         }
+//         return cnt;
+//     }
+// }
+
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        // Prefix sum 0 has occurred once
+        map.put(0, 1);
+        
+        int pfSum = 0;
+        int cnt = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            pfSum += nums[i];
+
+            int needed = pfSum - k;
+
+            // If needed prefix sum exists, add its frequency
+            if (map.containsKey(needed)) {
+                cnt += map.get(needed);
+            }
+
+            // Store/increase frequency of current prefix sum
+            map.put(pfSum, map.getOrDefault(pfSum, 0) + 1);
+        }
+
+        return cnt;
+    }
+}
