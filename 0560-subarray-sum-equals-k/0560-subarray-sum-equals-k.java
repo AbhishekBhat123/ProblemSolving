30fcb90ee34eccbfec03b8a37a51dd0d0fe5695f
Abhishek Bhat <144084687+AbhishekBhat123@users.noreply.github.com>
2026-08-30 22:39:46 +0530

    Time: 18 ms (99.79%), Space: 49.1 MB (22.76%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -16,31 +16,65 @@
 //     }
 // }
 
+// class Solution {
+//     public int subarraySum(int[] nums, int k) {
+        
+//         HashMap<Integer, Integer> map = new HashMap<>();
+        
+//         // Prefix sum 0 has occurred once
+//         map.put(0, 1);
+        
+//         int pfSum = 0;
+//         int cnt = 0;
+
+//         for (int i = 0; i < nums.length; i++) {
+//             pfSum += nums[i];
+
+//             int needed = pfSum - k;
+
+//             // If needed prefix sum exists, add its frequency
+//             if (map.containsKey(needed)) {
+//                 cnt += map.get(needed);
+//             }
+
+//             // Store/increase frequency of current prefix sum
+//             map.put(pfSum, map.getOrDefault(pfSum, 0) + 1);
+//         }
+
+//         return cnt;
+//     }
+// }
+
+
+
+
 class Solution {
     public int subarraySum(int[] nums, int k) {
         
         HashMap<Integer, Integer> map = new HashMap<>();
         
-        // Prefix sum 0 has occurred once
-        map.put(0, 1);
-        
-        int pfSum = 0;
+        int n = nums.length;
+        int pref[] = new int[n];
         int cnt = 0;
+        
+        pref[0] = nums[0];
+        for(int i = 1; i<n; i++){
+            pref[i] = pref[i-1] + nums[i];
+        }
 
-        for (int i = 0; i < nums.length; i++) {
-            pfSum += nums[i];
+        map.put(0,1);
 
-            int needed = pfSum - k;
-
-            // If needed prefix sum exists, add its frequency
-            if (map.containsKey(needed)) {
+        for(int i = 0; i<n; i++){
+            int needed = pref[i] - k;
+            if(map.containsKey(needed)){
                 cnt += map.get(needed);
             }
 
-            // Store/increase frequency of current prefix sum
-            map.put(pfSum, map.getOrDefault(pfSum, 0) + 1);
+            map.put(pref[i], map.getOrDefault(pref[i], 0)+1);
         }
 
         return cnt;
+
+            
     }
 }
