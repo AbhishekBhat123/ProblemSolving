f0d94ab049a1589940f50dd780db434f9bbf3e72
Abhishek Bhat <144084687+AbhishekBhat123@users.noreply.github.com>
2026-09-02 20:16:54 +0530

    Time: 66 ms (27.67%), Space: 48.1 MB (14.69%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,61 +1,89 @@
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+//         List<Character> list = new ArrayList<>();
+
+//         int n = s.length();
+//         // int max = Integer.MIN_VALUE;
+
+//         // for(int i =0; i<n; i++){
+//         //     if(!list.contains(s.charAt(i))){
+//         //         list.add(s.charAt(i));
+//         //         max = Math.max(max, (list.size()));
+//         //     }
+//         //     else{
+//         //         list.clear();
+//         //     }
+//         // }
+//         // return max;
+
+
+//         // HashMap<Character, Integer> hm = new HashMap<>();
+//         // int left = 0;
+//         // int right = 0;
+//         // int max = 0;
+
+//         // for(right = 0; right<n; right++){
+//         //     char ch = s.charAt(right);
+
+//         //     if(hm.containsKey(ch)){
+//         //         left = Math.max(left, hm.get(ch)+1);
+//         //     }
+
+//         //     hm.put(ch,right);
+//         //     max = Math.max(max, right - left + 1);
+
+//         // }
+
+//         // return max;
+
+
+
+//         int[] lastSeen = new int[128];
+//         Arrays.fill(lastSeen, -1);
+
+//         int left = 0;
+//         int maxLen = 0;
+
+//         for (int right = 0; right < s.length(); right++) {
+
+//             char ch = s.charAt(right);
+
+//             left = Math.max(left, lastSeen[ch] + 1);
+
+//             lastSeen[ch] = right;
+
+//             maxLen = Math.max(maxLen, right - left + 1);
+//         }
+
+//         return maxLen;
+   
+//     }
+// }
+
+
+
+
+
+
+
+// optimal
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        List<Character> list = new ArrayList<>();
-
         int n = s.length();
-        // int max = Integer.MIN_VALUE;
+        int start = 0;
+        int e = 0;
+        int max = 0;
+        HashSet<Character> hs = new HashSet<>();
+        for(e=0; e<n; e++){
+            while(hs.contains(s.charAt(e))){
+                hs.remove(s.charAt(start));
+                start++;
+            }
 
-        // for(int i =0; i<n; i++){
-        //     if(!list.contains(s.charAt(i))){
-        //         list.add(s.charAt(i));
-        //         max = Math.max(max, (list.size()));
-        //     }
-        //     else{
-        //         list.clear();
-        //     }
-        // }
-        // return max;
-
-
-        // HashMap<Character, Integer> hm = new HashMap<>();
-        // int left = 0;
-        // int right = 0;
-        // int max = 0;
-
-        // for(right = 0; right<n; right++){
-        //     char ch = s.charAt(right);
-
-        //     if(hm.containsKey(ch)){
-        //         left = Math.max(left, hm.get(ch)+1);
-        //     }
-
-        //     hm.put(ch,right);
-        //     max = Math.max(max, right - left + 1);
-
-        // }
-
-        // return max;
-
-
-
-        int[] lastSeen = new int[128];
-        Arrays.fill(lastSeen, -1);
-
-        int left = 0;
-        int maxLen = 0;
-
-        for (int right = 0; right < s.length(); right++) {
-
-            char ch = s.charAt(right);
-
-            left = Math.max(left, lastSeen[ch] + 1);
-
-            lastSeen[ch] = right;
-
-            maxLen = Math.max(maxLen, right - left + 1);
+            hs.add(s.charAt(e));
+            max = Math.max(max, e - start + 1);
         }
-
-        return maxLen;
+        return max;
    
     }
 }
