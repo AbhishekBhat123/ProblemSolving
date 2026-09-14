22228f5ac8ce11ef332ae8501e18d5c9f908feea
Abhishek Bhat <144084687+AbhishekBhat123@users.noreply.github.com>
2026-09-14 22:53:27 +0530

    Time: 3 ms (87.42%), Space: 52 MB (74.26%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,32 +1,65 @@
-// we are solving this using dfs method
+// // we are solving this using dfs method
+// class Solution {
+//     public int numIslands(char[][] grid) {
+//         int n = grid.length;
+//         int m = grid[0].length;
+//         int cnt = 0;
+
+//         for(int i = 0; i<n; i++){
+//             for(int j = 0; j<m; j++){
+//                 if(grid[i][j] == '1'){
+//                     cnt++;
+//                     dfs(grid, m, n,i, j);
+//                 }
+//             }
+//         }
+//         return cnt;
+//     }
+
+//     private void dfs(char grid[][], int m, int n, int i, int j){
+//         if(i<0 || i>=n || j<0 || j>=m || grid[i][j] == '0')
+//         {
+//             return;
+//         }
+
+//         grid[i][j] = '0';
+
+//         dfs(grid,m,n,i-1,j); //up
+//         dfs(grid,m,n,i+1,j); // down
+//         dfs(grid,m,n,i,j-1); // left
+//         dfs(grid,m,n,i,j+1); // right
+//     }
+// }
+
+
+
+ 
 class Solution {
     public int numIslands(char[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        int cnt = 0;
-
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<m; j++){
-                if(grid[i][j] == '1'){
-                    cnt++;
-                    dfs(grid, m, n,i, j);
-                }
+     int count = 0;
+     for(int i = 0; i<grid.length; i++){
+        for(int j = 0; j<grid[0].length; j++){
+            if(grid[i][j] == '1'){
+                count++;
+                dfs(grid, i, j);
             }
         }
-        return cnt;
+     }
+     return count;
     }
 
-    private void dfs(char grid[][], int m, int n, int i, int j){
-        if(i<0 || i>=n || j<0 || j>=m || grid[i][j] == '0')
-        {
+    private void dfs(char[][] grid, int i, int j){
+        int n = grid.length;
+        int m = grid[0].length;
+        if(i<0 || j<0 || i>=n || j>=m || grid[i][j] == '0'){
             return;
         }
 
         grid[i][j] = '0';
 
-        dfs(grid,m,n,i-1,j); //up
-        dfs(grid,m,n,i+1,j); // down
-        dfs(grid,m,n,i,j-1); // left
-        dfs(grid,m,n,i,j+1); // right
+        dfs(grid, i-1, j);
+        dfs(grid, i+1, j);
+        dfs(grid, i, j-1);
+        dfs(grid, i, j+1);
     }
 }
