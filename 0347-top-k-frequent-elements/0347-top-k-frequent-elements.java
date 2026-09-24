6ad8f100684f7a76e98948d65eeac4d2f1077e40
Abhishek Bhat <144084687+AbhishekBhat123@users.noreply.github.com>
2026-09-24 11:09:34 +0530

    Time: 11 ms (96.07%), Space: 47.5 MB (81.66%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,67 +1,32 @@
-// this is the brute force approach which i did
-// class Solution {
-//     public int[] topKFrequent(int[] nums, int k) {
-//         HashMap<Integer, Integer> map = new HashMap<>();
-//         for(int x: nums){
-//             map.put(x, map.getOrDefault(x,0)+1);
-//         }
-
-//         int ans[] = new int[k];
-//         int idx=0;
-//         int cnt = k;
-//         while(k-->0){
-//             int max = 0;
-//             int key = 0;
-//             for(int x: map.keySet()){
-//                if(map.get(x) > max){
-//                 max = map.get(x);
-//                 key = x;
-//                }
-//             }            
-//             ans[idx++] = key;
-//             map.remove(key);
-//         }
-//         return ans;
-//     }
-// }
-
-
-// Optimal uses: Bucket sort for to store freq
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
         for(int x: nums){
             map.put(x, map.getOrDefault(x,0)+1);
         }
-        //create bucket for to store freq
+        // freq: 1:3, 2:2, 3:1
         List<Integer>[] bucket = new ArrayList[nums.length + 1];
-
-        for(int key : map.keySet()){
-            int freq = map.get(key);
+        for(int x: map.keySet()){
+            int freq = map.get(x);
 
             if(bucket[freq] == null){
                 bucket[freq] = new ArrayList<>();
             }
-
-            bucket[freq].add(key);
+            bucket[freq].add(x);
         }
 
-        int ans[] = new int[k];
         int idx = 0;
+        int ans[] = new int[k];
+        for(int i = nums.length; i>=1 && k>0; i--){
+            if(bucket[i] != null){
+                for(int x: bucket[i]){
+                ans[idx++] = x;
+                k--;
 
-        for(int i =nums.length; i>=1 && k>0; i--){
-            if(bucket[i]!= null){
-
-                for(int x : bucket[i]){
-                    ans[idx++] = x;
-                    k--;
-
-                    if(k == 0){break;}
-                }
+                if(k==0){break;}
+                }                
             }
         }
-
         return ans;
-        
     }
 }
